f24d63c5faf4bc8f6441af534c427b2666a5731c
Ethan Kennedy <ejkennedy2003@gmail.com>
2024-02-07 23:52:28 +0000

    Chapter 7 Finished

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -57,9 +57,7 @@ ROOT_URLCONF = "tango_with_django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            TEMPLATE_DIR,
-        ],
+        "DIRS": [TEMPLATE_DIR,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,9 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATICFILES_DIRS = [STATIC_DIR,]
 
 STATIC_URL = "/static/"
 
